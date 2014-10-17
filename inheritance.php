ee13191724a53d1823ee4617cc4321de1e6c44fd
marlongaines <gainesmarlon@gmail.com>
2014-10-16 17:52:09 -0700

    inhertance.php

---
+++ inheritance.php
@@ -0,0 +1,40 @@
+<?php
+
+
+class animals
+{
+	protected $species;
+	protected $legs = 4;
+	protected $nose = 1;
+	protected $eyes = 2;
+	
+	protected function _construct() 
+	{
+		echo 'Your Species has been Registered';
+	}
+
+	
+}
+
+
+class dog extends animals
+{
+	
+	protected function _construct($species, $legs, $nose, $eyes) 
+	{
+		parent::_construct();
+		$this->species = $species;
+		$this->legs = $legs;
+		$this->nose = $nose;
+		$this->eyes = $eyes;
+	}
+	
+	
+	
+}
+
+$animal = new dog('pitbull', 4, 1, 2);
+
+
+
+?>
